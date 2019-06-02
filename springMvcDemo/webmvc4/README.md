0188d7f3afd2f0c19ba0866fe5283909445d6ce1
Jate Liao <3746826117@qq.com>
2019-06-02 20:30:10 +0800

    SpringMVC+SpringBoot的引入，下一步是springboot+dubbo+zipkin的集成搭建；

---
+++ springMvcDemo/webmvc4/README.md
@@ -0,0 +1,12 @@
+## WebMVC 4 Example
+
+### 运行步骤
+
+*    修改TracingConfiguration类中sender的值，配置对应的网关，用户名，用户key。
+
+*    启动前台服务$ mvn jetty:run -Pfrontend
+
+*    启动后台服务$ mvn jetty:run -Pbackend
+
+*    访问http://localhost:8081
+
---
+++ springMvcDemo/webmvc4/src/main/java/brave/webmvc/TracingConfiguration.java
@@ -0,0 +1,91 @@
+package brave.webmvc;
+
+import brave.Tracing;
+import brave.context.log4j2.ThreadContextScopeDecorator;
+import brave.http.HttpTracing;
+import brave.propagation.B3Propagation;
+import brave.propagation.ExtraFieldPropagation;
+import brave.propagation.ThreadLocalCurrentTraceContext;
+import brave.spring.web.TracingClientHttpRequestInterceptor;
+import brave.spring.webmvc.DelegatingTracingFilter;
+import brave.spring.webmvc.SpanCustomizingAsyncHandlerInterceptor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Sender;
+import zipkin2.reporter.okhttp3.OkHttpSender;
+
+/**
+ * This adds tracing configuration to any web mvc controllers or rest template clients.
+ *
+ * <p>This is a {@link Initializer#getRootConfigClasses() root config class}, so the
+ * {@linkplain DelegatingTracingFilter} added in {@link Initializer#getServletFilters()} can wire up
+ * properly.
+ */
+@Configuration
+// Importing these classes is effectively the same as declaring bean methods
+@Import({
+    TracingClientHttpRequestInterceptor.class,
+    SpanCustomizingAsyncHandlerInterceptor.class
+})
+public class TracingConfiguration extends WebMvcConfigurerAdapter {
+
+  /** Configuration for how to send spans to Zipkin */
+  @Bean Sender sender() {
+    /* endpoint 从https://tracing-analysis.console.aliyun.com/ 获取zipking网关(endpoint)
+    例如 http://tracing-analysis-dc-hz.aliyuncs.com/adapt_aokcdqnxyz@123456ff_abcdef123@abcdef123/api/v2/spans
+    */
+    return OkHttpSender.create("http://tracing-analysis-dc-hz.aliyuncs.com/adapt_aokcdqnxyz@123456ff_abcdef123@abcdef123/api/v2/spans");
+  }
+
+  /** Configuration for how to buffer spans into messages for Zipkin */
+  @Bean AsyncReporter<Span> spanReporter() {
+    return AsyncReporter.create(sender());
+  }
+
+  /** Controls aspects of tracing such as the name that shows up in the UI */
+  @Bean Tracing tracing(@Value("${zipkin.service:brave-webmvc4-example}") String serviceName) {
+    return Tracing.newBuilder()
+        .localServiceName(serviceName)
+        .propagationFactory(ExtraFieldPropagation.newFactory(B3Propagation.FACTORY, "user-name"))
+        .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder()
+            .addScopeDecorator(ThreadContextScopeDecorator.create()) // puts trace IDs into logs
+            .build()
+        )
+        .spanReporter(spanReporter()).build();
+  }
+
+  // decides how to name and tag spans. By default they are named the same as the http method.
+  @Bean HttpTracing httpTracing(Tracing tracing) {
+    return HttpTracing.create(tracing);
+  }
+
+  @Autowired RestTemplate restTemplate;
+  @Autowired TracingClientHttpRequestInterceptor clientInterceptor;
+
+  /** adds tracing to the application-defined rest template */
+  @PostConstruct public void init() {
+    List<ClientHttpRequestInterceptor> interceptors =
+        new ArrayList<>(restTemplate.getInterceptors());
+    interceptors.add(clientInterceptor);
+    restTemplate.setInterceptors(interceptors);
+  }
+
+  @Autowired SpanCustomizingAsyncHandlerInterceptor serverInterceptor;
+
+  /** adds tracing to the application-defined web controller */
+  @Override public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(serverInterceptor);
+  }
+}
